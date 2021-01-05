a08ac3e420adad2d194b511d5d266563b7957d21
aditiyadwiramadani88 <aditiyadwiramadani88@gamil.com>
2021-01-04 23:55:50 -0500

    Url

--- Musikal/urls.py
+++ Musikal/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from FrondEnd import views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -17,6 +16,5 @@ urlpatterns = [
     # jwt refresh token 
     path('resfresh_token', TokenRefreshView.as_view(), name='refresh_token'),
     #Frondend
-    path('ajax', views.main)
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
