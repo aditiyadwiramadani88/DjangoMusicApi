ad85dc0795b8bc90a2c78eb0de8817adf4a78956
aditiyadwiramadani88 <aditiyadwiramadani88@gamil.com>
2021-01-05 01:56:18 -0500

    Models

--- Music_Library/models.py
+++ Music_Library/models.py
@@ -6,11 +6,14 @@ import os
 # create table MUsicLib
 class MusicLib(models.Model):
     title = models.CharField(max_length=20)
-    song = models.FileField(validators=[FileExtensionValidator(['mp3'], 'allow fileeteoion mp3')])
+    song = models.FileField(validators=[FileExtensionValidator(['mp3'], 'allow file.mp3')])
     artist = models.CharField(max_length=25)
     def __str__(self):
         return f'{self.title}.{self.song}.{self.artist}'
     def delete(self, using=None, keep_parents=False):
         base_dir = 'Music_Library/static/Upload'
-        os.unlink(f'{base_dir}/{self.song}')
-        super().delete()
+        try: 
+             os.unlink(f'{base_dir}/{self.song}')
+             super().delete()
+        except Exception:
+            super().delete()
